6cfd8b4e5f832b022e8fa001d452e0d0a71c9f10
morefree0203 <1970840779@qq.com>
2025-07-23 11:49:08 +0800

    刚刚传掉了一个

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,18 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  // server: {
+  //   proxy: {
+  //     // 字符串简写写法
+  //     // '/foo': 'http://localhost:4567',
+  //     // 选项写法
+  //     '/teacher': {
+  //       target: 'http://localhost:8080', // 确保这是你后端服务的地址和端口
+  //       changeOrigin: true,
+  //       // 如果你的后端接口路径没有/api前缀，可以不用重写
+  //       // rewrite: (path) => path.replace(/^\/api/, '') 
+  //     },
+  //     // ... 其他需要代理的路径
+  //   }
+  // }
 })
